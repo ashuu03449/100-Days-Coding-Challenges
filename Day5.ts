e9508934343d86c6146e352f18c8698723f00f16
Ayesha <160266265+ashuu03449@users.noreply.github.com>
2024-03-05 11:24:24 -0800

    Update Day5.ts

--- Day5.ts
+++ Day5.ts
@@ -1,4 +1,4 @@
-//Day 04 Challenge
+//Day 05 Challenge
 //Question 13
 
 //Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.

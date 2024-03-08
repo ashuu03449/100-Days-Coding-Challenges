882dd59253684cc93c61e5296593ca61958e89d4
Ayesha <ashii14245@gmail.com>
2024-03-08 05:03:28 -0800

    coding-challenge

---
+++ Day7.js
@@ -0,0 +1,16 @@
+//Question 19
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+//Start Program exercise 14
+var guesttList = ["Ameen Alam", "Hina Naseer", "Emaan", "Ayesha", "Hafsa"];
+// guestList.forEach(inviteGuest => console.log(`Hello Dear ${inviteGuest}, you are invited to dinner plz join us.`));
+var lengthGuests = guesttList.length;
+console.log("We are Inviting total ".concat(lengthGuests, " guests."));
+// Question 20: Think of something you could store in an array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+var countries = ["North Korea", "Malaysia", "Japan", "Germany", "France"];
+console.log("Countries I'd like to visit: ", countries);
+var ProgrammingCourse = {
+    name: "Typescript & Javascript",
+    location: "Governor Sindh",
+    onsiteStudents: 50000
+};
+console.log(ProgrammingCourse);
---
+++ Day7.ts
@@ -0,0 +1,33 @@
+//Question 19
+
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+//Start Program exercise 14
+
+let guesttList = ["Ameen Alam", "Hina Naseer", "Emaan", "Ayesha", "Hafsa"];
+
+// guestList.forEach(inviteGuest => console.log(`Hello Dear ${inviteGuest}, you are invited to dinner plz join us.`));
+
+let lengthGuests: number = guesttList.length;
+
+console.log(`We are Inviting total ${lengthGuests} guests.`);
+
+// Question 20: Think of something you could store in an array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let countries: string[] = ["North Korea","Malaysia","Japan","Germany","France"]
+console.log("Countries I'd like to visit: ", countries);
+
+//Question 21
+interface programmingCourse {
+    name : string
+    location : string
+    onsiteStudents : number
+}
+
+let ProgrammingCourse:programmingCourse = {
+    name : "Typescript & Javascript",
+    location : "Governor Sindh",
+    onsiteStudents : 50000
+};
+
+console.log(ProgrammingCourse);

31fd6186001005f8981f428661153a0895075c97
Ayesha <160266265+ashuu03449@users.noreply.github.com>
2024-03-08 05:30:20 -0800

    Update Day7.ts

--- Day7.ts
+++ Day7.ts
@@ -1,7 +1,8 @@
 //Question 19
 
-// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
-
+/* Dinner Guests: Working with one of the programs from Exercises 14 through18,
+print a message indicating the number of people you are inviting to dinner.
+*/
 //Start Program exercise 14
 
 let guesttList = ["Ameen Alam", "Hina Naseer", "Emaan", "Ayesha", "Hafsa"];
@@ -12,7 +13,8 @@ let lengthGuests: number = guesttList.length;
 
 console.log(`We are Inviting total ${lengthGuests} guests.`);
 
-// Question 20: Think of something you could store in an array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+/*Question 20: Think of something you could store in an array. For example, you could make a list of mountains,
+rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.*/
 
 let countries: string[] = ["North Korea","Malaysia","Japan","Germany","France"]
 console.log("Countries I'd like to visit: ", countries);
